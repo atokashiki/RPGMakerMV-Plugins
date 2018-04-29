79cf896069a23e627d1bbe040b7a3f96cd3c55d9
Alan Tokashiki <artokashiki@gmail.com>
2018-04-29 15:45:27 -0700

    Add files via upload

--- ARTTitleCommandPosition.js
+++ ARTTitleCommandPosition.js
@@ -7,6 +7,36 @@ Testing how to write a plug-in by making a more user-friendly version of the Tit
 Alan Tokashiki 2018
 
 */
+/*:
+ * @plugindesc Changes the position of the title command window.
+ * @author Alan Tokashiki
+ *
+ * @param right
+ * @desc Moves title command to right.
+ * @default 0
+ *
+ * @param left
+ * @desc Moves title command to left.
+ * @default 0
+ *
+ * @param up
+ * @desc Moves title command up.
+ * @default 0
+ *
+ * @param down
+ * @desc Moves title command down.
+ * @default 0
+ *
+ * @param width
+ * @desc The width of the command window.
+ * @default 300
+ *
+ * @param background
+ * @desc The background type. 0: Normal, 1: Dim, 2: Transparent
+ * @default 0
+ *
+ * @help This plugin does not provide plugin commands.
+ */
 
 (function () {
 	
@@ -26,7 +56,7 @@ Alan Tokashiki 2018
 	Window_TitleCommand.prototype.updatePlacement = function() {
 		_Window_TitleCommand_updatePlacement.call(this);
 		this.x = this.x + right - left;
-		this.y = this.y + up - down;
+		this.y = this.y - up + down;
 		this.setBackgroundType(background);
 	};
 	
