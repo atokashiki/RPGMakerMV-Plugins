930c2d4b44735fb61007e6cb96216243b9f186c4
Alan Tokashiki <artokashiki@gmail.com>
2018-04-30 09:34:42 -0700

    Add files via upload

---
+++ ARTPluginTemplate.js
@@ -0,0 +1,37 @@
+// ARTPluginTemplate
+// ART RPGMaker Plug-in Master Template
+
+// Alan Tokashiki, 2018
+
+/*:
+ * @plugindesc The Plugin description from the RPGMaker Plugin menu
+ * @author Alan Tokashiki
+ *
+ * @param param1
+ * @desc parameter's description, provided in the Plugin menu
+ * @default param1_default_val
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * @help Any plugin commands you want to tell the user about
+ */
+
+ // Plugin's function here
+ // This is an anonymous function (has no name, just 'function')
+ // The final two parentheses make this a Self-Executing Anonymous Function
+ // Everything in the previous set of parentheses will be executed automatically!
+(function () {
+	
+	// STUFF STUFF STUFF here
+	
+}) ();
+
+// END ARTPluginTemplate
