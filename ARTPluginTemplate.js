748992768b4a7d6a58774704dfed721772ca2d9e
Alan Tokashiki <artokashiki@gmail.com>
2018-05-29 20:14:11 -0700

    Add files via upload

--- ARTPluginTemplate.js
+++ ARTPluginTemplate.js
@@ -5,15 +5,18 @@
 
 /*:
  * @plugindesc The Plugin description from the RPGMaker Plugin menu
+ * The Plugin description cuts off after two lines of text (autowraps)
+ *
  * @author Alan Tokashiki
  *
  * @param param1
  * @desc parameter's description, provided in the Plugin menu
- * @default param1_default_val
+ * The parameter description cuts off after two lines of text
+ * @default parameter's default value
  *
- *
- *
- *
+ * @param param2
+ * @desc another parameter! values are stored as strings, even if numbers!
+ * @default 0 (but as a string)
  *
  *
  *
@@ -30,8 +33,18 @@
  // Everything in the previous set of parentheses will be executed automatically!
 (function () {
 	
-	// STUFF STUFF STUFF here
+	// LOADING PARAMETERS INTO PLUGIN:
+	// must return an instance of the parameters and store in a variable
+	// Use the PluginManager's parameters() function, with the title of THIS pluging as argument
+	var parameters = PluginManager.parameters('ARTPluginTemplate');
+	// parameters is basically an array storing all plugin's parameters(from above)
 	
+	// ACCESSING A PARAMETER:
+	// can access any parameters by name, stored in parameters variable
+	// can store parameters in separate variables as well
+	var param1 = parameters['param1'];
+	var param2 = parameters['param2'];
+	// These variables will be returned as STRINGS
 }) ();
 
 // END ARTPluginTemplate
